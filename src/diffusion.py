61b5898ec9847d294dcd46cb415c9822589e45d6
NatnaelTigistu <habtethiop@gmail.com>
2026-07-16 01:52:32 +0300

    Change beta scheduling from lenear to cosine

--- src/diffusion.py
+++ src/diffusion.py
@@ -1,9 +1,19 @@
 import torch
+import math
 import torch.nn.functional as F
 
 
-def linear_beta_schedule(timesteps, beta_start=1e-4, beta_end=0.02):
-    return torch.linspace(beta_start, beta_end, timesteps)
+def cosine_beta_schedule(timesteps, s=0.008):
+    steps = timesteps + 1
+    x = torch.linspace(0, timesteps, steps)
+
+    alphas_cumprod = (
+        torch.cos(((x / timesteps) + s) / (1 + s) * math.pi * 0.5) ** 2
+    )
+    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+
+    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    return torch.clip(betas, 0.0001, 0.9999)
 
 
 class Diffusion:
@@ -22,7 +32,7 @@ class Diffusion:
         self.timesteps = timesteps
         self.device = device
 
-        self.betas = linear_beta_schedule(timesteps).to(device)
+        self.betas = cosine_beta_schedule(timesteps).to(device)
         self.alphas = 1.0 - self.betas
         self.alphas_cumprod = torch.cumprod(self.alphas, dim=0)
 
